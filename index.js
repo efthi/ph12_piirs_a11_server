227b69217c48187f676a850198f7c289bea0a097
Md Eftequarul Alam <efthaqur@gmail.com>
2025-12-08 23:42:00 +0600

    Add: MongoDB Connection, test get and post checked

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
- * Importing express and cors 
+ * Importing express and cors
  **/
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 //console.log(process.env.DB_USER);
 
 /**
@@ -18,20 +18,70 @@ app.use(cors());
 /**
  * Declare Port!
  */
-const port = process.env.PORT || 3000;  
-
+const port = process.env.PORT || 3000;
 
 /**
  * Port Listen Declaration
  */
 
-app.listen(port, (res)=>{
-    console.log(`SERVER RUNNING @ ${port}`);
-    
+app.listen(port, (res) => {
+  console.log(`SERVER RUNNING @ ${port}`);
 });
 
 /**......................Express Config Ends....................... */
 
-app.get('/', (req, res) => {
-    res.send('Express Server is Running!');
-})
+/**......................Database Config Starts....................... */
+//MongoDB এর কাজ শুরু, প্রথমে MongoDB import করবো
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+//mongoDB এর URI সেট করা হলো .env থেকে
+const uri = process.env.MONGODB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+/**......................Database Config Ends....................... */
+
+/**......................API Section Starts....................... */
+async function run() {
+  try {
+    //উপরে MongoDB এর client কে কানেক্ট করলাম
+    await client.connect();
+    const piirsDB = client.db("piirsDB"); //database বানাইলাম, থাকলে আবার ক্রিয়েট হবে না
+    const issueData = piirsDB.collection("base"); // collection বানাইলাম, ....
+    const userData = piirsDB.collection("userData"); //২য় collection বানাইলাম
+    //Connection test code
+    await client.db("admin").command({ ping: 1 });
+    console.log("pinged your deployment. Connected to MongoDB!");
+
+    app.get("/", (req, res) => {
+      res.send("Express Server is Running!");
+    });
+
+    app.get("/get-test", async (req, res) => {
+      const packet = issueData.find(); //packet জাস্ট নাম দেওয়া হয়েছে
+      const result = await packet.toArray();
+      res.send(result);
+    });
+
+    app.post("/get-post", async (req, res) => {
+      const newIssue = req.body;
+      console.log(newIssue);
+      const result = await issueData.insertOne(newIssue);
+      res.send(result);
+    });
+
+    app.get("/", (req, res) => {
+      res.send("Express Server is Running!");
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+/**......................API Section Ends....................... */
+
+//run ফাংশনকে কল করলাম
+run().catch(console.dir);
